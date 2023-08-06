3df4f7a216d9a070258a1ed7e2eaf2150093a627
atanu sarkar <atanu.sarkar@bosenet.com>
2023-08-06 23:22:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+let js="amazing";
+      if (js === "amazing") {alert("JS is Fun!")};
+      
+let age = 21
+console.log(age)
+
+const firstname = "Atanu";
+const age = 21;
+const job = "software developer";
+
+// const showstr = "I'm "+firstname+ " my age is "+age+" years."+" I am a "+job;
+const showstr = `I'm ${firstname} my age is ${age} years. I am a ${job}`
+console.log(showstr);
+
+//Without templete literal
+console.log ('String with \n\
+multiple \n\
+lines');
+//With templete literal
+console.log (`String with 
+multiple 
+lines`);*/
+
+//IF-ELSE
+const age = 23;
+const ischeck = age>=18;
+if(ischeck){
+    console.log(`elegible for driving 🚗`)
+}
+else{
+    console.log(`Not eligible for driving 🚗`)
+}
